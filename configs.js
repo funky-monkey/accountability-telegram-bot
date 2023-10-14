c5c3356c5ea7abd0d41f4e4a4b432d4c207a7a58
Sidney de Koning <sidney.dekoning@gmail.com>
2023-10-14 21:21:07 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -3,7 +3,7 @@ const configs = {
   MY_NAME: "Sidney",
   // Repeated daily tasks (these tasks will be automatically added each day)
   // an example of the syntax:
-  DAILY_REPEATED_TASKS: ["Opstaan om 7.00 uur", "Inchecken bij Fem om 8.00 uur", "Aan het werk om 8.15", "10 bladzijde lezen uit een boek", "10 minuten mediteren na opstaan", "10 minuten workout gewichten, bodyweight of dumbels (thuis of in gym)", "Elke maandag 5 solliciaties vinden en versturen"],
+  DAILY_REPEATED_TASKS: ["Opstaan om 7.00 uur", "Inchecken bij Fem om 8.00 uur", "Aan het werk om 8.15", "Pomodoro timer zetten voor taken", "10 bladzijde lezen uit een boek", "10 minuten mediteren na opstaan", "10 minuten workout gewichten, bodyweight of dumbels (thuis of in gym)", "Elke maandag 5 solliciaties vinden en versturen"],
 
   // When you want to be greeted by the bot (to start your day)
   GREETING_TIME: {
